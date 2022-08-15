bc4db0b8d03be50a29660c515ff13d6381b74848
Islambeg <islambeg@protonmail.com>
2022-08-15 23:19:10 +0300

    Add PrimeVue UI kit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from "vue";
+import PrimeVue from "primevue/config";
 import App from "./App.vue";
 import "./assets/styles/_main.scss";
+import "primevue/resources/themes/nova-vue/theme.css";
+import "primevue/resources/primevue.min.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.mount("#app");
