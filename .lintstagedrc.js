10f35d16a61739e0bd9e8d9f79f8f813bed9da88
Islambeg <islambeg@protonmail.com>
2022-08-15 22:55:42 +0300

    Add stylelint and autoprefixer

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,6 @@
+/* eslint-env node */
 module.exports = {
+  "*.{css,scss,html,vue}": "npm run stylelint --",
   "*.{vue,js,cjs,mjs}": "npm run lint --",
   "*.{json,vue,js,cjs,mjs}": "npm run prettify --",
 };
---
+++ .stylelintrc.js
@@ -0,0 +1,18 @@
+/* eslint-env node */
+const namingPattern = "^([a-zA-Z][a-zA-Z0-9]*)([-_]{0,2}[a-zA-Z0-9]+)*$";
+
+module.exports = {
+  extends: ["stylelint-config-standard-scss", "stylelint-config-prettier-scss"],
+  overrides: [
+    {
+      files: ["**/*.html", "**/*.vue"],
+      customSyntax: "postcss-html",
+    },
+  ],
+  rules: {
+    "selector-class-pattern": namingPattern,
+    "custom-property-pattern": namingPattern,
+    "scss/dollar-variable-pattern": namingPattern,
+    "value-keyword-case": ["lower", { ignoreProperties: ["composes"] }],
+  },
+};
